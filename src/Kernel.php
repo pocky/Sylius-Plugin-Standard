46ee596357fc91850013e417e4d94fde19dadf4b
Alexandre Balmes <alexandre@vanoix.com>
2021-03-02 12:46:46 +0100

    Migrate to 1.9

--- src/Kernel.php
+++ src/Kernel.php
@@ -21,7 +21,8 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
 
-final class Kernel extends BaseKernel
+/** @final */
+class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
@@ -39,6 +40,7 @@ final class Kernel extends BaseKernel
 
     public function registerBundles(): iterable
     {
+        /** @psalm-suppress UnresolvableInclude */
         $contents = require $this->getProjectDir() . '/config/bundles.php';
         foreach ($contents as $class => $envs) {
             if (isset($envs['all']) || isset($envs[$this->environment])) {
